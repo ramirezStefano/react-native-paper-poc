0f8107aa32d51a3b178313563ffa012a3c46ba75
Stefano Ramirez <ramirez.stefano@gmail.com>
2023-06-02 14:57:30 -0600

    add stable version implementing react-native-paper library.

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
+import * as React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import {
+  MD3LightTheme as DefaultTheme,
+  PaperProvider,
+  Button,
+  TextInput,
+  Card,
+  Avatar,
+  Checkbox,
+  RadioButton,
+  List,
+  MD3Colors,
+  Menu,
+  Divider,
+} from "react-native-paper";
 
 export default function App() {
+  //const LeftContent = (props) => <Avatar.Icon {...props} icon="alien" />;
+
+  //useState for the checkbox and state .
+  // const [checked, setChecked] = React.useState(false);
+  // const [checkedRadio, setCheckedRadio] = React.useState("first");
+
+  //stated for the menu
+  // const [visible, setVisible] = React.useState(false);
+  // const openMenu = () => setVisible(true);
+  // const closeMenu = () => setVisible(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      {/* view wrapper for the menu implementation. */}
+
+      <View style={styles.container}>
+        <Text>React-Native-Page POC on Expo!</Text>
+        <Text>stable test!</Text>
+      </View>
+      <ScrollView>
+        <View>
+          <Button icon="alien"></Button>
+          <Button icon="triforce"></Button>
+          <Button icon="cheese"></Button>
+          <Button icon="bacteria"></Button>
+        </View>
+      </ScrollView>
+    </PaperProvider>
   );
 }
 
