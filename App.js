25276c25bc599cd06c95a8db34965f1ea5b88c40
Stefano Ramirez <ramirez.stefano@gmail.com>
2023-06-13 01:04:05 -0600

    prototype header component

--- App.js
+++ App.js
@@ -1,110 +1,43 @@
 import * as React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet } from "react-native";
 import {
   MD3LightTheme as DefaultTheme,
   PaperProvider,
-  Button,
-  TextInput,
-  Card,
   Avatar,
-  Checkbox,
-  RadioButton,
-  List,
-  MD3Colors,
-  Menu,
-  Divider,
-  Appbar,
+  Card,
 } from "react-native-paper";
 
 export default function App() {
-  //const LeftContent = (props) => <Avatar.Icon {...props} icon="alien" />;
-
-  //useState for the checkbox and state .
-  // const [checked, setChecked] = React.useState(false);
-  // const [checkedRadio, setCheckedRadio] = React.useState("first");
-
-  //stated for the menu
-  const [visible, setVisible] = React.useState(false);
-  const openMenu = () => setVisible(true);
-  const closeMenu = () => setVisible(false);
-
-  //state to use input
-  const [text, setText] = React.useState("");
+  const LeftContent = (props) => (
+    <Avatar.Image
+      {...props}
+      size={100}
+      style={{
+        borderRadius: 8,
+        marginLeft: -30,
+        borderColor: "transparent",
+      }}
+      source={{ uri: "https://picsum.photos/700" }} // Replace with your desired image URLr
+    />
+  );
 
   return (
     <PaperProvider>
-      {/* view wrapper for the menu implementation. */}
-
-      {/* Appbar component here for the TopBar */}
-      <Appbar.Header>
-        <Appbar.BackAction onPress={() => {}} />
-        <Appbar.Content title="Appbar Header!" />
-        <Appbar.Action icon="rocket" onPress={() => {}} />
-      </Appbar.Header>
-
-      <View
-        style={{ padding: 50, flexDirection: "row", justifyContent: "center" }}
-      >
-        <Menu
-          visible={visible}
-          onDismiss={closeMenu}
-          anchor={
-            <Button icon="cheese" onPress={openMenu}>
-              Show Cool Menu
-            </Button>
-          }
-        >
-          <Menu.Item onPress={() => {}} title="Item 1" />
-          <Menu.Item onPress={() => {}} title="Item 2" />
-          <Divider />
-          <Menu.Item onPress={() => {}} title="Item 3" />
-        </Menu>
-      </View>
-      <View style={styles.container}>
-        <Text>React-Native-Page POC on Expo!</Text>
-        <Text>stable test!</Text>
-      </View>
-      <ScrollView>
-        <View>
-          <Button
-            mode="contained"
-            icon="alien"
-            onPress={() => console.log("Pressed Alien")}
-          ></Button>
-          <Button
-            mode="contained"
-            icon="triforce"
-            onPress={() => console.log("You found the Triforce!")}
-          ></Button>
-          <Button
-            mode="elevated"
-            icon="cheese"
-            onPress={() => console.log("Have some Cheese! ")}
-          ></Button>
-          <Button
-            mode="outlined"
-            icon="bacteria"
-            onPress={() => console.log("This is NOT a bug!")}
-          ></Button>
-          {/* for rendering testing */}
-          <Button
-            mode="text"
-            icon="alien"
-            onPress={() => console.log("Another one?")}
-          ></Button>
-        </View>
-
-        <View>
-          <TextInput
-            mode="flat"
-            label="Say hello!"
-            onChangeText={(text) => setText(text)}
-            value={text}
-            textColor="blue"
-          />
-        </View>
-      </ScrollView>
+      <Card style={{ padding: 45 }} mode="elevated">
+        <Card.Title
+          title="Business Name"
+          subtitle="Business Address"
+          left={LeftContent}
+          titleStyle={{
+            marginLeft: 40,
+            fontWeight: "bold",
+            marginTop: -40,
+            marginBottom: 2,
+          }}
+          subtitleStyle={{ marginLeft: 40, opacity: 0.6 }}
+        />
+      </Card>
     </PaperProvider>
   );
 }
